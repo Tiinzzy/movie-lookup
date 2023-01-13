91398e0947d8d4ac8d61bc7b4231771db4a813d7
Tina <tinzzy.cracked@yahoo.com>
2023-01-13 18:18:50 -0500

    code cleanup

--- src/components/back-end/ConnectingToMySql.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def open_database():
-    con = mysql.connector.connect(user='dbadmin', password='washywashy',host='127.0.0.1',database='tests')
-    return con, con.cursor()
-
-def close_database(con, cursor):
-    cursor.close()
-    con.close()
