237727c40dc3a5c2e0747f38f88a2baba9fb05d0
Tina <tinzzy.cracked@yahoo.com>
2023-01-14 13:11:44 -0500

    changed name of component, mapped data

---
+++ src/components/front-end/CarouselSlider.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import StarIcon from '@mui/icons-material/Star';
+
+import './style.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import BackEndConnection from './BackEndConnection';
+const backend = BackEndConnection.INSTANCE();
+
+export default class CarouselSlider extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    async componentDidMount() {
+        let movies = await backend.get_movies();
+        this.setState({ randomMovies: movies })
+        console.log(movies)
+    }
+
+    render() {
+        var settings = {
+            dots: true,
+            infinite: false,
+            speed: 500,
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            initialSlide: 0,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 6,
+                        slidesToScroll: 1,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
+
+        return (
+            <div>
+                <Slider {...settings}>
+                    {this.state.randomMovies && this.state.randomMovies.map((e, i) =>
+                        <div>
+                            <Box className="EachMovieBox">
+                                <Box className="MovieTitleVoteBox">
+                                    <Typography variant="h6" fontWeight="bold" style={{ fontSize: 14 }} key={i}>{e.title}</Typography>
+                                    <span style={{ color: '#F5C518', marginLeft: 4, marginRight: 2 }}><StarIcon /></span>
+                                    <span style={{ fontSize: 14 }}>{e.vote}</span>
+                                </Box>
+                                <Box className="GenreImdbBox">
+                                    <a href={'https://www.imdb.com/title/' + e.imdb} target="_blank" id='genreClick'> Check on IMDB </a>
+                                    <a target="_blank" href="#" id='genreClick' style={{ marginTop: 8 }}> Genres </a>
+                                </Box>
+                            </Box>
+                        </div>)}
+                </Slider>
+            </div>
+        );
+    }
+}
+
+
+
+
+
+
