7d987d5bffd49ce3c257c57d30b62f8935e5ef7e
Tina <tinzzy.cracked@yahoo.com>
2023-01-14 13:12:24 -0500

    getting more data from database

--- backend-api/Movies.py
+++ backend-api/Movies.py
@@ -13,11 +13,11 @@ class Movies:
     def get_movies(self):
         db = Database()
         con, cur = db.open_database()
-        cur.execute("SELECT m.title, m.vote_average, m.overview FROM tests.imbd_movies m limit 10")
+        cur.execute("SELECT m.title, m.vote_average, m.overview, m.vote_count, m.imdb_id FROM tests.imbd_movies m ORDER BY RAND() limit 12;")
         rows = cur.fetchall()
         result = []
         for row in rows:
-            result.append({'title': row[0], 'vote': row[1], 'overview': row[2]})
+            result.append({'title': row[0], 'vote': row[1], 'overview': row[2], 'vote_count': row[3], 'imdb': row[4]})
         db.close_database()        
         return result
 
