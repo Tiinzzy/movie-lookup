71ce1daa34617806601728ee918acc1e8a7852f3
Tina <tinzzy.cracked@yahoo.com>
2023-01-14 19:21:36 -0500

    mapped each genre separately by splitting by comma

--- src/components/front-end/CarouselSlider.js
+++ src/components/front-end/CarouselSlider.js
@@ -12,13 +12,13 @@ import "slick-carousel/slick/slick-theme.css";
 import BackEndConnection from './BackEndConnection';
 const backend = BackEndConnection.INSTANCE();
 
-const TITLE_LENGTH = 16;
+const TITLE_LENGTH = 14;
 
-function niceTitle(t){
-    if (t.length < TITLE_LENGTH){
+function niceTitle(t) {
+    if (t.length < TITLE_LENGTH) {
         return t;
     } else {
-        return t.substring(0,TITLE_LENGTH) + ' ...'
+        return t.substring(0, TITLE_LENGTH) + ' ...'
     }
 }
 
@@ -80,14 +80,20 @@ export default class CarouselSlider extends Component {
                             <Box className="EachMovieBox">
                                 <Box className="MovieTitleVoteBox">
                                     <Typography title={e.title} variant="h6" fontWeight="bold" style={{ fontSize: 14 }} key={i}>{niceTitle(e.title)}</Typography>
-                                    <span style={{ color: '#F5C518', marginLeft: 4, marginRight: 2 }}><StarIcon /></span>
+                                    {/* <Box flexGrow={1} /> */}
+                                    <span className="StarSpan"><StarIcon /></span>
                                     <span style={{ fontSize: 14 }}>{e.vote}</span>
                                 </Box>
                                 <Box className="GenreImdbBox">
                                     <a href={'https://www.imdb.com/title/' + e.imdb} target="_blank" id='genreClick'>
-                                        <img src="/imdb.png" height="12"/>
+                                        <img src="/imdb.png" height="12" alt="#" />
                                     </a>
-                                    {/* <a target="_blank" href="#" id='genreClick' style={{ marginTop: 8 }}> Genres </a> */}
+                                    <span className="GenresSlider">Genres:</span>
+                                    <Box className="EachGenreBox">
+                                        {e.genres.split(',').map((g, i) => (
+                                            <a key={i} title={e.genres} target="_blank" href="#" id='genreClick' className="GenreLink">{g}</a>
+                                        ))}
+                                    </Box>
                                 </Box>
                             </Box>
                         </div>)}
