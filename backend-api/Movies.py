cc3a5587b987198940376a7b04be87a127698443
Tina <tinzzy.cracked@yahoo.com>
2023-01-14 19:21:11 -0500

    getting genres from database sending to frontend

--- backend-api/Movies.py
+++ backend-api/Movies.py
@@ -1,5 +1,6 @@
 from Database import Database
 
+
 class Movies:
     def __init__(self):
         pass
@@ -8,17 +9,20 @@ class Movies:
     def search_movies(self, keywords):
         return ['Toy Story', 'God Father', 'Joker']
 
-
     @classmethod
     def get_movies(self):
         db = Database()
         con, cur = db.open_database()
-        cur.execute("SELECT m.title, m.vote_average, m.overview, m.vote_count, m.imdb_id FROM tests.imbd_movies m ORDER BY RAND() limit 12;")
+        cur.execute(''' SELECT m.title, m.vote_average, m.overview, m.vote_count, m.imdb_id, mal.genre_count FROM tests.imbd_movies m 
+                        join tests.movies_all_genres mal on mal.title = m.title
+                        ORDER BY RAND() limit 12;
+                        ''')
         rows = cur.fetchall()
         result = []
         for row in rows:
-            result.append({'title': row[0], 'vote': row[1], 'overview': row[2], 'vote_count': row[3], 'imdb': row[4]})
-        db.close_database()        
+            result.append(
+                {'title': row[0], 'vote': row[1], 'overview': row[2], 'vote_count': row[3], 'imdb': row[4], 'genres': row[5]})
+        db.close_database()
         return result
 
 
