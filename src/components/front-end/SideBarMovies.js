3c5e64eaf0fcf475d94f7031ae28bdf92fbd4fd0
Tina <tinzzy.cracked@yahoo.com>
2023-01-16 09:04:58 -0500

    cleaned up tagline and change top 10 based on changing genre

--- src/components/front-end/SideBarMovies.js
+++ src/components/front-end/SideBarMovies.js
@@ -6,9 +6,20 @@ import Typography from "@mui/material/Typography";
 import BackEndConnection from './BackEndConnection';
 import TopTenMoviesGenre from './TopTenMoviesGenre';
 
+import { shared } from './functions';
+
 import './style.css';
 
 const backend = BackEndConnection.INSTANCE();
+
+function cleanUp(tag) {
+    if (tag === '0') {
+        return 'No Tagline Available';
+    } else {
+        return tag;
+    }
+
+}
 
 class SideBarMovies extends React.Component {
     constructor(props) {
@@ -16,11 +27,22 @@ class SideBarMovies extends React.Component {
         this.state = {
 
         };
+
+        this.callSideBarMovies = this.callSideBarMovies.bind(this);
+        shared.callSideBarMovies = this.callSideBarMovies;
     }
 
     async componentDidMount() {
         let topTen = await backend.get_top_movies('');
         this.setState({ topMovies: topTen });
+    }
+
+    async callSideBarMovies(message) {
+        if (message.action === 'genre-has-been-selected') {
+            let topTen = await backend.get_top_movies(message.data);
+            console.log(topTen)
+            this.setState({ topMovies: topTen });
+        }
     }
 
     render() {
@@ -33,7 +55,7 @@ class SideBarMovies extends React.Component {
                 {this.state.topMovies && this.state.topMovies.map((e, i) =>
                     <Box className="EachSideMovieBox" key={i}>
                         <Typography variant="body1" fontWeight='600'>{e.title}</Typography>
-                        <Typography variant="body1" fontWeight='300' style={{ paddingTop: 5, borderTop: 'solid 1px rgb(215, 215, 215)' }}>"{e.tagline}"</Typography>
+                        <Typography variant="body1" fontWeight='300' style={{ paddingTop: 5, borderTop: 'solid 1px rgb(215, 215, 215)' }}>"{cleanUp(e.tagline)}"</Typography>
                     </Box>)}
             </Box>
         );
