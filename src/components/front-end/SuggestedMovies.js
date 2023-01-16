e83ac47ee9ff3387cf66fd485bb4d71cb26e9024
Tina <tinzzy.cracked@yahoo.com>
2023-01-16 18:09:20 -0500

    renamed for another component

--- src/components/front-end/SuggestedMovies.js
+++ src/components/front-end/SuggestedMovies.js
@@ -10,12 +10,13 @@ import './style.css';
 
 const backend = BackEndConnection.INSTANCE();
 
-class SearchResuts extends React.Component {
+class SuggestedMovies extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
 
         };
+        this.movieSelected = this.movieSelected.bind(this);
     }
 
     async componentDidMount() {
@@ -23,11 +24,17 @@ class SearchResuts extends React.Component {
         this.setState({ randomMovies: movies })
     }
 
+    async movieSelected(e) {
+        let data = await backend.get_selected_movie(e);
+        console.log(data)
+
+    }
+
     render() {
         return (
             <Box className="SuggestedMoviesMainBox" style={{ width: window.innerWidth - 300 }}>
                 {this.state.randomMovies && this.state.randomMovies.map((e, i) =>
-                    <Box key={i} mb={2}>
+                    <Box key={i} mb={2} onClick={() => this.movieSelected(e.movie_id)} style={{ cursor: 'pointer' }}>
                         <Box className="MovieTitleBox">
                             <Typography variant="h6" fontWeight="bold" style={{ display: 'inline-block' }} key={i}>{e.title}</Typography>
                             <span className="VoteStyle"><StarIcon /></span>{e.vote}<span className="VoteCountStyle">({e.vote_count})</span>
@@ -43,4 +50,4 @@ class SearchResuts extends React.Component {
     }
 }
 
-export default SearchResuts;
+export default SuggestedMovies;
