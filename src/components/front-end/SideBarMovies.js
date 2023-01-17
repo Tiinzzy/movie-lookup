15bd3369797f6e104d068f7a68cf1e6f375ac5e3
Tina <tinzzy.cracked@yahoo.com>
2023-01-17 09:15:17 -0500

    clicking on movie displays the full detail of the movie

--- src/components/front-end/SideBarMovies.js
+++ src/components/front-end/SideBarMovies.js
@@ -26,13 +26,14 @@ class SideBarMovies extends React.Component {
         this.state = {
 
         };
-
+        this.movieSelected = this.movieSelected.bind(this);
         this.callSideBarMovies = this.callSideBarMovies.bind(this);
         shared.callSideBarMovies = this.callSideBarMovies;
     }
 
     async componentDidMount() {
         let topTen = await backend.get_top_movies('');
+        // console.log(topTen)
         this.setState({ topMovies: topTen });
     }
 
@@ -49,13 +50,18 @@ class SideBarMovies extends React.Component {
         }
     }
 
+    async movieSelected(e) {
+        let data = await backend.get_selected_movie(e);
+        shared.callSearchResult({ action: 'selected_movie_data_recieved', movie: data });
+    }
+
     render() {
         return (
             <Box className="SideMvvies">
                 <Typography variant="h6" fontWeight='bold' mb={1} fontSize={20}> Top Rated Movies</Typography>
                 <TopTenMoviesGenre />
                 {this.state.topMovies && this.state.topMovies.map((e, i) =>
-                    <Box className="EachSideMovieBox" key={i}>
+                    <Box className="EachSideMovieBox" key={i} onClick={() => this.movieSelected(e.id)} style={{ cursor: 'pointer' }}>
                         <Typography variant="body1" fontWeight='600'>{e.title}</Typography>
                         <Typography variant="body1" fontWeight='300' style={{ paddingTop: 5, borderTop: 'solid 1px rgb(215, 215, 215)' }}>"{cleanUp(e.tagline)}"</Typography>
                     </Box>)}
