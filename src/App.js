f89a2a1fac0ae45eef3659c35b972621cb944435
Tina <tinzzy.cracked@yahoo.com>
2023-01-17 18:00:29 -0500

    fixed problem with router and getting data from query

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Home from './components/front-end/Home';
+
+
 import Body from './components/front-end/Body';
 import MovieClicked from './components/front-end/MovieClicked';
 
+const queryParameters = new URLSearchParams(window.location.search)
+
 export default function App() {
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Body />} />
-      <Route path="/Home" element={<Body />} />
-      <Route path="/Movie_Clicked" element={<MovieClicked />} />
-      <Route path='*' element={<h3>Error</h3>} />
-    </Routes>
-  </BrowserRouter>
+  let movie_id = queryParameters.get("movie_id")
 
   return (
     <>
-      <Body />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/movie-clicked/" element={<MovieClicked movie_id={movie_id} />} />
+          <Route path='*' element={<h3>Error</h3>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
