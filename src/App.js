4af55035e7aa3037f616efc043f47eefc3a9af38
Tina <tinzzy.cracked@yahoo.com>
2023-01-17 09:05:40 -0500

    created route paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Body from './components/front-end/Body';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Body from './components/front-end/Body';
+import SearchResult from './components/front-end/SearchResult';
+
+export default function App() {
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Body />} />
+      <Route path="/Home" element={<Body />} />
+      <Route path="/Movie_Clicked" element={<SearchResult />} />
+      <Route path='*' element={<h3>Error</h3>} />
+    </Routes>
+  </BrowserRouter>
+
   return (
     <>
       <Body />
     </>
   );
-}
-
-export default App;
+};
