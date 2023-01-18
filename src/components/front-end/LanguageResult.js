2a30d5344e33f743db847e53845ca548b63f2e63
Tina <tinzzy.cracked@yahoo.com>
2023-01-18 11:18:26 -0500

    display data received from backend

--- src/components/front-end/LanguageResult.js
+++ src/components/front-end/LanguageResult.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import Typography from "@mui/material/Typography";
+import StarIcon from '@mui/icons-material/Star';
+import Pagination from '@mui/material/Pagination';
+import LinearProgress from '@mui/material/LinearProgress';
 import Box from "@mui/material/Box";
 
+import BackEndConnection from './BackEndConnection';
+
 import './style.css';
+
+const backend = BackEndConnection.INSTANCE();
+const PAGE_SIZE = 6;
 
 function getPageCount(rowCount, pageSize) {
     let pageCount = Math.floor(rowCount / pageSize);
@@ -17,8 +26,25 @@ class LanguageResult extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selected_language: props.selected_language
+            selected_language: props.selected_language,
+            showProgress: false
         };
+    }
+
+    async componentDidMount() {
+        this.getDataForDisplay(1);
+    }
+
+    async getDataForDisplay(e) {
+        this.setState({ showProgress: true }, async function () {
+            let pageNumber = (e - 1) * 6;
+            let languageResult = await backend.get_movies_based_on_spoken_languages(this.state.selected_language, pageNumber);
+            this.setState({
+                showProgress: false,
+                randomMovies: languageResult.rows,
+                length: getPageCount(languageResult.row_count, PAGE_SIZE)
+            });
+        });
     }
 
 
@@ -26,7 +52,33 @@ class LanguageResult extends React.Component {
     render() {
         return (
             <Box>
-                
+                {this.state.showProgress ?
+                    <Box sx={{ color: 'grey.500' }}><LinearProgress color="inherit" /></Box> :
+                    <div style={{ height: 4 }}></div>
+                }
+                <Box style={{ display: 'flex', flexDirection: 'column', padding: 45 }}>
+                    {this.state.randomMovies && this.state.randomMovies.map((e, i) =>
+                        <Box key={i} mb={2} style={{ cursor: 'pointer' }}>
+                            <Box className="MovieTitleBox">
+                                <Typography variant="h6" fontWeight="bold" style={{ display: 'inline-block' }}>{e.title}</Typography>
+                                <span className="VoteStyle"><StarIcon /></span>{e.vote}<span className="VoteCountStyle">({e.vote_average})</span>
+                            </Box>
+                            <Box className="MovieOverviewBox">
+                                <Typography variant="body1" mb={2}>
+                                    {e.overview}
+                                </Typography>
+                                <span style={{ fontWeight: 'bold', fontSize: 14, marginRight: 6 }}>Available Languages:</span>
+                                {e.languages.split(',').map((g, i) => (
+                                    <Typography key={i} variant="caption" style={{ marginRight: 4, fontSize: 12, color: g === this.state.selected_country ? 'red' : 'black' }}>
+                                        {g}
+                                    </Typography>))}
+                            </Box>
+                        </Box>)}
+                    <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                        <Pagination count={this.state.length} onChange={(e, i) => this.getDataForDisplay(i)} />
+                    </Box>
+                </Box>
+
             </Box>
         );
     }
