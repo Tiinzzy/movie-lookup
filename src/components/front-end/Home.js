2ba0ad4ce3331fb9b2cd484367f7316df92dfe62
Tina <tinzzy.cracked@yahoo.com>
2023-01-18 07:57:03 -0500

    moved components for home in the home component

--- src/components/front-end/Home.js
+++ src/components/front-end/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+import Box from "@mui/material/Box";
+
+import SuggestedMovies from './SuggestedMovies';
+import CarouselSlider from './CarouselSlider';
+import SideBarMovies from './SideBarMovies';
 
 import './style.css';
 
@@ -15,9 +20,17 @@ class Home extends React.Component {
     render() {
         return (
             <>
-
-                THIS SHOULD BE HOME
-
+                <Box display='flex'>
+                    <Box style={{ width: 500 }}>
+                        <SideBarMovies />
+                    </Box>
+                    <Box flexGrow={1}>
+                        <SuggestedMovies />
+                    </Box>
+                </Box>
+                <Box className="SLiderBox">
+                    <CarouselSlider />
+                </Box>
             </>
         );
     }
