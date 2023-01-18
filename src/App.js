c4e8848b2d497f7123707e17b0812d07e1de7417
Tina <tinzzy.cracked@yahoo.com>
2023-01-18 09:45:28 -0500

    adding route for country result and adding component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Home from './components/front-end/Home';
 import Footer from "./components/front-end/Footer";
 import Header from './components/front-end/Header';
 import MovieClicked from './components/front-end/MovieClicked';
-import SearchResult from "./components/front-end/SearchResult";
+import GenreResult from "./components/front-end/GenreResult";
+import CountryResult from "./components/front-end/CountryResult";
 
 const queryParameters = new URLSearchParams(window.location.search)
 
 export default function App() {
   let movie_id = queryParameters.get("movie_id");
   let selected_genre = queryParameters.get("selected_genre");
+  let selected_country = queryParameters.get("selected_country");
+  console.log(selected_country)
 
   return (
     <>
@@ -21,7 +24,8 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/movie-clicked/" element={<MovieClicked movie_id={movie_id} />} />
-          <Route path="/search-result" element={<SearchResult selected_genre={selected_genre}/>} />
+          <Route path="/genre-result" element={<GenreResult selected_genre={selected_genre}/>} />
+          <Route path="/country-result" element={<CountryResult selected_country={selected_country}/>} />
           <Route path='*' element={<h3>Error</h3>} />
         </Routes>
       </BrowserRouter>
