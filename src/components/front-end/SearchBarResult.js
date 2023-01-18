2f221e777836b30cb3c5eb847408a13604a7ede0
Tina <tinzzy.cracked@yahoo.com>
2023-01-18 14:06:30 -0500

    component to display the search result

---
+++ src/components/front-end/SearchBarResult.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Box from "@mui/material/Box";
+
+import { shared } from "./functions";
+
+import './style.css';
+
+class SearchBarResult extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searched_item: props.searched_item
+        };
+    }
+
+    render() {
+        return (
+            <Box>
+
+            </Box>
+        );
+    }
+}
+
+export default SearchBarResult;
