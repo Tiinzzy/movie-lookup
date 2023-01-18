e605205b0fa9920e3c052cc552e209322c6664d4
Tina <tinzzy.cracked@yahoo.com>
2023-01-18 07:54:35 -0500

    moved footer and header component in app.js to have it at display as default in any page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './components/front-end/Home';
-
-
-import Body from './components/front-end/Body';
+import Footer from "./components/front-end/Footer";
+import Header from './components/front-end/Header';
 import MovieClicked from './components/front-end/MovieClicked';
 
 const queryParameters = new URLSearchParams(window.location.search)
@@ -14,14 +13,16 @@ export default function App() {
 
   return (
     <>
+      <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Body />} />
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/movie-clicked/" element={<MovieClicked movie_id={movie_id} />} />
           <Route path='*' element={<h3>Error</h3>} />
         </Routes>
       </BrowserRouter>
+      <Footer />
     </>
   );
 };
