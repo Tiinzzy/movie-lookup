6cdc492d5580ca19a5047061f75119253ee273cf
Tina <tinzzy.cracked@yahoo.com>
2023-01-19 15:18:12 -0500

    moved duplicate functions there

--- src/components/front-end/functions.js
+++ src/components/front-end/functions.js
@@ -1,3 +1,29 @@
+const TITLE_LENGTH = 12;
+
 export const shared = {
     initial_render_count: 0   
+}
+
+export function cleanUp(tag) {
+    if (tag === '0') {
+        return 'No Tagline Available';
+    } else {
+        return tag;
+    }
+}
+
+export function getPageCount(rowCount, pageSize) {
+    let pageCount = Math.floor(rowCount / pageSize);
+    if (pageCount * pageSize < rowCount) {
+        pageCount += 1;
+    }
+    return pageCount;
+}
+
+export function niceTitle(t) {
+    if (t.length < TITLE_LENGTH) {
+        return t;
+    } else {
+        return t.substring(0, TITLE_LENGTH) + ' ...'
+    }
 }
