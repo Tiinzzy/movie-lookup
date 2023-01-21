c73b07c413fa5a1d88551fcb43d65e47cb018d89
Tina <tinzzy.cracked@yahoo.com>
2023-01-21 16:20:33 -0500

    code cleanup

--- src/components/front-end/messaging.js
+++ src/components/front-end/messaging.js
@@ -1,10 +1,9 @@
 var ee = require('event-emitter');
 
-
 var MyClass = function () { };
 ee(MyClass.prototype);
-export var emitter = new MyClass();
 
+export var emitter = new MyClass();
 
 export const LISTENERS = {
     getHeader: () => document.getElementById('header-serach-box')
