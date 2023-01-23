9408f3971741dc48a4671cb1bf97e44920061bed
Tina <tinzzy.cracked@yahoo.com>
2023-01-23 13:06:13 -0500

    changed the format of receiving the data from top 10 genres

--- src/components/front-end/SideBarMovies.js
+++ src/components/front-end/SideBarMovies.js
@@ -6,10 +6,10 @@ import Typography from "@mui/material/Typography";
 import BackEndConnection from './BackEndConnection';
 import TopTenMoviesGenre from './TopTenMoviesGenre';
 
-import { shared, cleanUp } from './functions';
+import { cleanUp } from './functions';
 
 import './style.css';
-import { emitter } from './messaging';
+import { emitter, LISTENERS } from './messaging';
 
 const backend = BackEndConnection.INSTANCE();
 
@@ -20,8 +20,8 @@ class SideBarMovies extends React.Component {
 
         };
         this.movieSelected = this.movieSelected.bind(this);
-        this.callSideBarMovies = this.callSideBarMovies.bind(this);
-        shared.callSideBarMovies = this.callSideBarMovies;
+        // this.callSideBarMovies = this.callSideBarMovies.bind(this);
+        // shared.callSideBarMovies = this.callSideBarMovies;
     }
 
     componentDidMount() {
@@ -30,23 +30,21 @@ class SideBarMovies extends React.Component {
             that.setState({ topMovies: data });
         });
 
-    }
-
-    callSideBarMovies(message) {
-        if (message.action === 'genre-has-been-selected') {
-            if (message.data === '- ALL -') {
-                let that = this;
-                backend.get_top_movies('', function (data) {
-                    that.setState({ topMovies: data });
-                });
-            } else {
-                let that = this;
-                backend.get_top_movies(message.data, (data) => {
-                    that.setState({ topMovies: data });
-                });
+        LISTENERS.getSelectedGenre().addEventListener('new-genre-has-been-selected',
+            (e) => {
+                if (e.detail.data === '- ALL -') {
+                    let that = this;
+                    backend.get_top_movies('', function (data) {
+                        that.setState({ topMovies: data });
+                    });
+                } else {
+                    let that = this;
+                    backend.get_top_movies(e.detail.data, (data) => {
+                        that.setState({ topMovies: data });
+                    });
+                }
             }
-
-        }
+            , false);
     }
 
     movieSelected(e) {
@@ -54,17 +52,13 @@ class SideBarMovies extends React.Component {
     }
 
     sendToSearch(title) {
-        // const event = new CustomEvent('search-this-text', {
-        //     detail: { title, time: new Date() }
-        // });
-        // LISTENERS.getHeader().dispatchEvent(event);
 
         emitter.emit('search-this-text', { title, date: new Date() });
     }
 
     render() {
         return (
-            <Box className="SideMovies">
+            <Box className="SideMovies" id="side-movies-box">
                 <Typography variant="h6" fontWeight='bold' mb={1} fontSize={16}> Top Rated Movies</Typography>
                 <TopTenMoviesGenre />
                 {this.state.topMovies && this.state.topMovies.map((e, i) =>
