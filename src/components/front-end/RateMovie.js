3b2d878c486e2bcc01acbb8b731ba67569f12948
Tina <tinzzy.cracked@yahoo.com>
2023-01-23 12:09:35 -0500

    fixed issue with movie title, getting from backend with movie id provided by parent

--- src/components/front-end/RateMovie.js
+++ src/components/front-end/RateMovie.js
@@ -11,10 +11,12 @@ import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import Button from "@mui/material/Button";
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContentText from '@mui/material/DialogContentText';
+
+import BackEndConnection from './BackEndConnection';
 
 import './style.css';
+
+const backend = BackEndConnection.INSTANCE();
 
 class RateMovie extends React.Component {
     constructor(props) {
@@ -22,18 +24,25 @@ class RateMovie extends React.Component {
         this.state = {
             openDialog: false,
             value: 0,
-            title: props.title,
+            id: props.id,
             submittedValue: 0
         };
-        console.log(props.title)
+
         this.openRatingMovie = this.openRatingMovie.bind(this);
         this.closeDialog = this.closeDialog.bind(this);
         this.selectRating = this.selectRating.bind(this);
         this.submitRating = this.submitRating.bind(this);
     }
 
+    componentDidMount() {
+        let that = this;
+        backend.get_selected_movie(this.state.id, (data) => {
+            that.setState({ title: data[0].title });
+        });
+    }
+
     openRatingMovie() {
-        this.setState({ openDialog: true, title: this.state.title });
+        this.setState({ openDialog: true });
     }
 
     closeDialog() {
@@ -45,13 +54,18 @@ class RateMovie extends React.Component {
     }
 
     submitRating(e) {
-        this.setState({ openDialog: false, submittedValue: e });
+        this.setState({ submittedValue: e, openDialog: false }, function () {
+            let that = this;
+            backend.submit_rating(this.state.submittedValue, function (data) {
+                that.setState({});
+            });
+        });
     }
 
     render() {
         return (
             <>
-                <Box>
+                <Box id="movie-rating-star">
                     <IconButton onClick={() => this.openRatingMovie()}>
                         <StarHalfIcon fontSize="large" id="RateMovieRatingStar" />
                     </IconButton>
@@ -66,17 +80,18 @@ class RateMovie extends React.Component {
                             <Typography style={{ textAlign: 'center', fontSize: 18 }}>Rate</Typography>
                             <Typography style={{ textAlign: 'center', fontSize: 25, fontWeight: 'bold' }}>{this.state.title}</Typography>
                         </DialogTitle>
-                        <Divider mt={1} mb={1}/> 
+                        <Divider mt={1} mb={1} />
                         <DialogContent>
-                        <Box style={{ display: 'flex', flexDirection: 'column' }}>
-                            <Rating
-                                name="customized-10"
-                                value={this.state.value}
-                                max={10}
-                                onChange={(e, i) => this.selectRating(i)}
-                            />
-                            <Button variant="contained" id="SubmitRatingButton" onClick={() => this.submitRating(this.state.value)}>Rate</Button>
-                        </Box>
+                            <Box style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
+                                <Rating
+                                    name="customized-10"
+                                    value={this.state.value}
+                                    max={10}
+                                    onChange={(e, i) => this.selectRating(i)}
+                                />
+                                <br />
+                                    {/* <Button variant="contained" id="SubmitRatingButton" onClick={() => this.submitRating(this.state.value)}>Rate</Button> */}
+                            </Box>
                         </DialogContent>
                     </Dialog>
 
