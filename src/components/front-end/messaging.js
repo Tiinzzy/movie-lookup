cb43c23be6f8fdd33592a927d9087dad4d712790
Tina <tinzzy.cracked@yahoo.com>
2023-01-24 08:58:10 -0500

    changed receiving box name so i can use it among other components as well

--- src/components/front-end/messaging.js
+++ src/components/front-end/messaging.js
@@ -9,7 +9,7 @@ export const LISTENERS = {
     getHeader: () => document.getElementById('header-serach-box'),
     getSelectedGenre: () => document.getElementById('side-movies-box'),
     getToCloseMenu: () => document.getElementById('header-menu-box'),
-    getUpdateVotes: () => document.getElementById('movie-clicked-box')
+    getUpdateVotes: () => document.getElementById('update-movie-vote-average-box')
 }
 
 
